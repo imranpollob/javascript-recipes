e3750a5183fc8f3839551b1bbe06f5acd9078979
pollmix <polboy77@gmail.com>
2018-05-08 13:01:08 +0600

    template literals

---
+++ template literals.js
@@ -0,0 +1,12 @@
+let name = "Pollob";
+let experience = 2;
+let today = new Date();
+
+
+let output = `Hello,
+What's Up?
+
+My name is ${name}.
+I have ${experience} years experience till ${today.toLocaleString()}`;
+
+console.log(output);
