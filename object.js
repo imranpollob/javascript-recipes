e1211c467c2e129b26e94c0d609e58aee0184c72
pollmix <polboy77@gmail.com>
2018-05-10 11:42:02 +0600

    basic object

---
+++ object.js
@@ -0,0 +1,13 @@
+const Person = {
+    name : "Imran Pollob",
+    age : 25,
+     
+    info(){
+      return `Name: ${this.name} Age: ${this.age}`;
+    }
+  };
+  
+
+  const firstPerson = Person.info();
+  
+  console.log(firstPerson);  
