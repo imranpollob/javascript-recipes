86fcc4cfc2c6e73ba26d62a29fa75d0938ce88c5
pollmix <polboy77@gmail.com>
2018-05-10 12:46:25 +0600

    Update

--- class alternative.js
+++ class alternative.js
@@ -1,18 +1,36 @@
-const Person = function(name){
-    
+const Person = function(name, language, likes = []){
     this.name = name;
-    this.language = "Bengali";
+    this.language = language;
+    this.likes = likes;
     
     this.info = function(){
         return `${this.name} talks in ${this.language}`;
     }
     
-};
-
-const firstPerson = new Person("Pollob");
-
-firstPerson.age = 25;
-
-console.log(firstPerson);
-console.log(firstPerson.name);
-console.log(firstPerson.info());
+  };
+  
+  Person.prototype.secret = function(){
+    return `${this.name} likes ${this.likes}`;
+  }
+  
+  Person.prototype.setSecret = function(secret){
+    let likings = "";
+    secret.forEach(like => likings += " "+like);
+  
+    this.likes = likings;
+  }
+  
+  const pollob = new Person("Pollob", "Bengali");
+  const tanji = new Person("Tanji", "English");
+  
+  tanji.age = 25;
+  
+  console.log(tanji.info());
+  console.log(tanji.age);
+  
+  console.log(pollob.name);
+  console.log(pollob.info());
+  
+  pollob.setSecret(["Ice Cream", "Gaming"]);
+  
+  console.log(pollob.secret());
